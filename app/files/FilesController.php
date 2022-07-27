2ede9a02c65e1333d00fb55d613a8f87b7e3e08f
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2022-07-27 13:56:42 -0300

    Update FilesController.php
    update solicitud corregida

--- app/files/FilesController.php
+++ app/files/FilesController.php
@@ -33,42 +33,101 @@ class FilesController extends BaseController
     {
         if ($this->getRequestMethod() == "POST") {
             // var_dump($_FILES);
+            // var_dump($params);
+            // die();
             $objService = new FilesService;
-            $tamaño = $objService->validarSizeArchivos($_FILES);
-            $extension = $objService->validarExtensionArchivos($_FILES);
-            if (isset($tamaño)) {
-                if (isset($extension)) {
-                    $objServiceVecino = new VecinoService;
-                    $insertVecino = $objServiceVecino->insertVecino($params);
-                    if ($insertVecino != -1) {
-                        $params['vecino_id'] = $insertVecino;
-                        $objServiceSolicitud = new SolicitudService;
-                        $insertSolicitud = $objServiceSolicitud->insertSolicitud($params);
-                        if ($insertSolicitud != -1) {
-                            $idSolicitud= $insertSolicitud;
-                            $arrPath=[];
-                            foreach ($_FILES as $key => $value) {
-                                $nombreArchivo = "solicitud_" . $idSolicitud . "-" . $key . obtenerExtensionArchivo($value['type']);
-                                //$nombreArchivo = "licencia_" . $this->getIdTramite() . "_" . $params['descripcionArchivo'];
-                                $filePathSolicitud = getDireccionArchivoAdjunto("RMAMH", $nombreArchivo, $idSolicitud);
-                                $objService->subirArchivoServidor($value['tmp_name'], $value['type'], $value['size'], $filePathSolicitud);
-                                $arrPath[$key]=$filePathSolicitud;
-                                //Actualizar path de archivos en solicitud por cada archivo armar array de paths y update todo de una
+            $objServiceSolicitud = new SolicitudService;
+            if (array_key_exists("esEdicion", $params)) {
+                $solicitudHistorial = $objServiceSolicitud->selectSolicitudPorID($params);
+                // $insertSolicitudHistorico = $objServiceSolicitud->insertSolicitudHistorico($solicitudHistorial);
+                if (isset($insertSolicitudHistorico) || true) {
+                    if (count($_FILES) > 0) {
+                        $tamaño = $objService->validarSizeArchivos($_FILES);
+                        $extension = $objService->validarExtensionArchivos($_FILES);
+                        if (isset($tamaño)) {
+                            if (isset($extension)) {
+                                $insertSolicitud = $objServiceSolicitud->updateRevisionSolicitud($params,$solicitudHistorial);
+                                if (isset($insertSolicitud)) {
+                                    $idSolicitud = $params['id_solicitud'];
+                                    $arrPath = [];
+                                    foreach ($_FILES as $key => $value) {
+                                        $nombreArchivo = "solicitud_" . $idSolicitud . "-" . $key . obtenerExtensionArchivo($value['type']);
+                                        //$nombreArchivo = "licencia_" . $this->getIdTramite() . "_" . $params['descripcionArchivo'];
+                                        $filePathSolicitud = getDireccionArchivoAdjunto("RMAMH", $nombreArchivo, $idSolicitud);
+                                        if(file_exists($filePathSolicitud.$nombreArchivo)){
+                                            unlink($filePathSolicitud.$nombreArchivo);
+                                        }
+                                        $objService->subirArchivoServidor($value['tmp_name'], $value['type'], $value['size'], $filePathSolicitud);
+                                        $arrPath[$key] = $filePathSolicitud;
+                                        //Actualizar path de archivos en solicitud por cada archivo armar array de paths y update todo de una
+                                    }
+                                    $data = $objServiceSolicitud->updatePathSolcituModificacion($idSolicitud, $arrPath);
+                                    $response = crearRespuestaSolicitud(200, "OK", "Solicitud Subida", $data);
+                                } else {
+                                    $response = crearRespuestaSolicitud(400, "error", "no se puedo registrar la solicitud");
+                                }
+                            } else {
+                                $response = crearRespuestaSolicitud(400, "error", $extension);
                             }
-                            $data=$objServiceSolicitud->updatePathSolcitud($idSolicitud,$arrPath);
-                            $response = crearRespuestaSolicitud(200, "OK", "Solicitud Subida",$data);
                         } else {
-                            $response = crearRespuestaSolicitud(400, "error", "no se puedo registrar la solicitud");
+                            $response = crearRespuestaSolicitud(400, "error", $tamaño);
                         }
-                    } else {
-                        $response = crearRespuestaSolicitud(400, "error", "no se puedo registrar vecino");
+                    }else{
+                        // var_dump($solicitudHistorial);
+                        // var_dump($params);
+                        // die();
+                        // $params[""]
+                        $insertSolicitud = $objServiceSolicitud->updateRevisionSolicitud($params,$solicitudHistorial);
+                        if ($insertSolicitud != 0) {
+                            $response = crearRespuestaSolicitud(200, "OK", "Solicitud Subida correctamente.");
+                        } else {
+                            $response = crearRespuestaSolicitud(400, "Error", "No se ha podido enviar la solicitud.");
+                        }
+                        $response['headers'] = ['HTTP/1.1 200 OK'];
+                        #../../../projects_files/RMAMH/
+                        // $objServiceSolicitud->updatePathSolcitud($insertSolicitud, $arrPath);
                     }
                 } else {
-                    $response = crearRespuestaSolicitud(400, "error", $extension);
+                    $response = crearRespuestaSolicitud(400, "error", "No se puedo registrar historial de solicitud");
                 }
             } else {
 
-                $response = crearRespuestaSolicitud(400, "error", $tamaño);
+                $tamaño = $objService->validarSizeArchivos($_FILES);
+                $extension = $objService->validarExtensionArchivos($_FILES);
+                if (isset($tamaño)) {
+                    if (isset($extension)) {
+                        $objServiceVecino = new VecinoService;
+                        $insertVecino = $objServiceVecino->insertVecino($params);
+                        if ($insertVecino != -1) {
+                            $params['vecino_id'] = $insertVecino;
+                            $objServiceSolicitud = new SolicitudService;
+                            $insertSolicitud = $objServiceSolicitud->insertSolicitud($params);
+                            if ($insertSolicitud != -1) {
+                                $idSolicitud = $insertSolicitud;
+                                $arrPath = [];
+                                foreach ($_FILES as $key => $value) {
+                                    $nombreArchivo = "solicitud_" . $idSolicitud . "-" . $key . obtenerExtensionArchivo($value['type']);
+                                    //$nombreArchivo = "licencia_" . $this->getIdTramite() . "_" . $params['descripcionArchivo'];
+                                    $filePathSolicitud = getDireccionArchivoAdjunto("RMAMH", $nombreArchivo, $idSolicitud);
+                                    $objService->subirArchivoServidor($value['tmp_name'], $value['type'], $value['size'], $filePathSolicitud);
+                                    $arrPath[$key] = $filePathSolicitud;
+                                    //Actualizar path de archivos en solicitud por cada archivo armar array de paths y update todo de una
+                                }
+                                $data = $objServiceSolicitud->updatePathSolcitud($idSolicitud, $arrPath);
+                                $response = crearRespuestaSolicitud(200, "OK", "Solicitud Subida", $data);
+                            } else {
+                                $response = crearRespuestaSolicitud(400, "error", "no se puedo registrar la solicitud");
+                            }
+                        } else {
+                            $response = crearRespuestaSolicitud(400, "error", "no se puedo registrar vecino");
+                        }
+                    } else {
+                        $response = crearRespuestaSolicitud(400, "error", $extension);
+                    }
+                } else {
+
+                    $response = crearRespuestaSolicitud(400, "error", $tamaño);
+                }
             }
         } else {
             //No se utilizo el metodo HTTP correcto
