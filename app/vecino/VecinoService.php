49f4663f951208711abcf1d9e53b01f4ca97e458
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2022-09-05 10:29:37 -0300

    Update VecinoService.php
    fix metodos de recuperacion de datos del vecino, modificacion e insercion

--- app/vecino/VecinoService.php
+++ app/vecino/VecinoService.php
@@ -4,12 +4,11 @@ class VecinoService
 {
     public function insertVecino($params)
     {
-        $fechaNacimiento = formatearFechaNacimiento($params['fechaNacimiento']);
         $params['urlFoto']=($params['urlFoto']==="null")?null:$params['urlFoto'];
         $params['telefonoAlternativo']=($params['telefonoAlternativo']==="null")?null:$params['telefonoAlternativo'];
         $params['emailAlternativo']=($params['emailAlternativo']==="null")?null:$params['emailAlternativo'];
-        $sqlQuery = "INSERT INTO RMAMH_Vecino (wap_persona, nombre, apellido, tipo_documento, documento, fecha_nacimiento, email, telefono, url_foto, codigo_postal, provincia, ciudad, domicilio,telefonoAlternativo,emailAlternativo) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-        $bindParams = [$params['wap_persona'], $params['nombre'], $params['apellido'], $params['tipoDocumento'], $params['documento'], $fechaNacimiento, $params['email'], $params['telefono'], $params['urlFoto'], $params['codigoPostal'], $params['provincia'], $params['ciudad'], $params['domicilio'],$params['telefonoAlternativo'],$params['emailAlternativo']];
+        $sqlQuery = "INSERT INTO RMAMH_Vecino (wap_persona, url_foto, codigo_postal, provincia, ciudad, domicilio,telefonoAlternativo,emailAlternativo) VALUES(?,?,?,?,?,?,?,?)";
+        $bindParams = [$params['wap_persona'], $params['urlFoto'], $params['codigoPostal'], $params['provincia'], $params['ciudad'], $params['domicilio'],$params['telefonoAlternativo'],$params['emailAlternativo']];
 
         $database = new BaseDatos;
         $database->connect();
@@ -27,15 +26,15 @@ class VecinoService
         return $database->ejecutarSqlUpdateDelete($sqlQuery, $bindParams);
     }
     public function obtenerIdVecino($params){
-        $sqlQuery = "SELECT id_vecino FROM RMAMH_Vecino WHERE wap_persona=? OR documento=?";
-        $bindParams = [$params['wap_persona'], $params['documento']];
+        $sqlQuery = "SELECT id_vecino,Nombre,CorreoElectronico FROM RMAMH_Vecino INNER JOIN wapPersonas ON RMAMH_Vecino.wap_persona = wapPersonas.ReferenciaID WHERE wap_persona=?";
+        $bindParams = [$params['wap_persona']];
 
         $database = new BaseDatos;
         $database->connect();
         return $database->ejecutarSqlSelect($sqlQuery, $bindParams);
     }
     public function obtenerDatosVecino($params){
-        $sqlQuery = "SELECT telefonoAlternativo,emailAlternativo FROM RMAMH_Vecino WHERE wap_persona=? OR documento=?";
+        $sqlQuery = "SELECT telefonoAlternativo,emailAlternativo FROM RMAMH_Vecino WHERE wap_persona=?";
         $bindParams = [$params['wap_persona'], $params['documento']];
 
         $database = new BaseDatos;
